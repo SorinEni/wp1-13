5744860b5ea1ad715f16a2a6d2d7bd8cf45b5702
SorinEni <60381859+SorinEni@users.noreply.github.com>
2020-05-24 15:48:24 +0200

    j

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="submit.php" method="post">
+
+    <label for="first-number"> Zadej první číslo:  </label>
+    <input type="number" name="firstnumber" id="">
+
+ <br>
+
+    <label for="second-number"> Zadej druhé číslo:  </label>
+    <input type="number" name="secondnumber" id="">
+
+ <br>
+
+    <input type="submit" value="Odeslat">
+
+ <br>
+
+    </form>
+</body>
+</html>
---
+++ submit.php
@@ -0,0 +1,34 @@
+<?php
+$firstnumber = filter_input(INPUT_POST, 'firstnumber');
+$secondnumber = filter_input(INPUT_POST, 'secondnumber');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+
+
+
+
+
+<p> <?= $firstnumber ?> + <?= $secondnumber ?> = <?= $firstnumber + $secondnumber ?> </p>
+<p> <?= $firstnumber ?> - <?= $secondnumber ?> = <?= $firstnumber - $secondnumber ?> </p>
+<p> <?= $firstnumber ?> * <?= $secondnumber ?> = <?= $firstnumber * $secondnumber ?> </p>
+<p> <?= $firstnumber ?> / <?= $secondnumber ?> = <?= $firstnumber / $secondnumber ?> </p>
+
+
+
+
+
+
+
+
+</body>
+</html>
